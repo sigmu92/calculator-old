0653c433ab3640e3684a62b1f285039417c1a4c6
Sigmu92 <maxwellw92@gmail.com>
2020-07-06 17:16:21 -0400

    Order of operations implemented, potential for infinite loop

--- myScript.js
+++ myScript.js
@@ -5,7 +5,8 @@ const OPR = document.querySelectorAll('.opr-button')
 const EQUAL = document.querySelector('.equal-button')
 const CLEAR = document.querySelector('.clear-button')
 
-let results = false;
+let results = false; //Tracks if results are being show
+let needNum = true; //Tracks if number is required to be entered, or if operator entry is okay
 let inputs = [];
 
 function add(a,b){
@@ -38,6 +39,7 @@ function operate(a,b,operator){
 }
 
 function getNum(){
+    needNum = false; //Operator entry okay now
     if (results == false){ 
         DISPLAY.textContent+=`${this.value}`
     }
@@ -48,33 +50,82 @@ function getNum(){
 }
 
 function getOpr(){
+    if (needNum == true) {
+        DISPLAY.textContent='ERROR';
+    } else{
     inputs.push(Number(DISPLAY.textContent));
     DISPLAY.textContent='';
     inputs.push(this.value);
-
+    needNum = true;
+    }
 }
 
 function calculate(){
+
     inputs.push(Number(DISPLAY.textContent));
-    let total = 0;
-    let a,b,operator;
-    console.log(inputs)
-    a=inputs.shift();
-    operator=inputs.shift();
-    b=inputs.shift();
-    console.log(b);
-    console.log(operator);
+    let reference = 0; //reference to reinsert total into input array
+    let total = 0; //total value, being calculated
+    let findPri = false;
+    if (inputs.length%2==0){
+        DISPLAY.textContent="ERROR"
+        return;
+    }
+    while (inputs.length>1){
+        //Loops until inputs has been reduced to a total
+        for (let i=0; i<inputs.length; i++){
+            if (inputs[i]=='*' || inputs[i]=='/'){
+                reference = i-1;
+                findPri = true;
+                break;
+            }
+        }
+        if (findPri == true){
+            let sentence = inputs.splice(reference,3);
+            total = collapseInput(sentence);
+            if (total=="Div"){
+                return;
+            }
+            findPri=false;
+        } else {
+            for (let i=0; i<inputs.length; i++){
+                if (inputs[i]=='+' || inputs[i]=='-'){
+                    reference = i-1;
+                    break;
+                }
+            }
+            let sentence = inputs.splice(reference,3);
+            total = collapseInput(sentence);
+            if (total=="Div"){
+                return;
+            }
+        }   
+        inputs.splice(reference,0,total)
+    }
+
+    DISPLAY.textContent=`${inputs[0]}`;
+    inputs=[];
+    results = true;
+}   
+function clearInputs(){
+    DISPLAY.textContent='';
+    inputs=[];
+}
+
+function collapseInput(sentence) {
+    const a = sentence[0];
+    const operator = sentence[1];
+    const b = sentence[2];
     if (b==0 && operator =='/'){
         DISPLAY.textContent = "ERROR"
         inputs = [];
-        return;
+        return("Div");
     } 
-    total+=operate(a,b,operator);
-    DISPLAY.textContent=`${total}`;
-    results = true;
-}   
-
+    const total=operate(a,b,operator);
+    return(total);
+}
 
 NUMBERS.forEach((button) => button.addEventListener('click', getNum));
 OPR.forEach((button) => button.addEventListener('click', getOpr))
 EQUAL.addEventListener('click', calculate)
+CLEAR.addEventListener('click', clearInputs)
+
